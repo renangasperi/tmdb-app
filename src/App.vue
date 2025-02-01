1fa76fba0e64e0463b537f4036a7dc72aa97bc50
Renan Gasperi <renangasperi@gmail.com>
2025-02-01 17:31:51 -0300

    initial commit with header

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="app h-screen w-full bg-gray-100">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the Home Page</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Hello, Vue 3!");
+
+function greet() {
+  console.log(message.value);
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
